33be4efe52d68bf78f3e293436a738b1146f1e24
laesmie <laesmie@gmail.com>
2020-05-02 16:24:37 -0700

    Final Updates

---
+++ js/javascript-functions.js
@@ -0,0 +1,7 @@
+var document;
+var today = new Date();
+var y = today.getFullYear();
+document.getElementById('year').innerHTML = y;
+
+var update = document.lastModified;
+document.getElementById('lastUpdated').innerHTML = update;
