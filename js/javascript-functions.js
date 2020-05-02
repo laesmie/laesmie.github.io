0801bed79cd57e4a34dc77c05ff878df42350d04
laesmie <laesmie@gmail.com>
2020-05-02 16:35:59 -0700

    Maybe I'm done now?

--- js/javascript-functions.js
+++ js/javascript-functions.js
@@ -1,7 +1,7 @@
 var document;
 var today = new Date();
 var y = today.getFullYear();
-document.getElementById('year').innerHTML = y;
+document.getElementById('current-year').innerHTML = y;
 
 var update = document.lastModified;
-document.getElementById('lastUpdated').innerHTML = update;
+document.getElementById('update-time').innerHTML = update;
