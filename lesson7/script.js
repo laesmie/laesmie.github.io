078a28f651269d2658b9da06b764d78d6f486634
Use your GitHub username <Enter your email address>
2020-06-04 16:29:16 -0700

    morechanges

--- lesson7/script.js
+++ lesson7/script.js
@@ -1,27 +1,26 @@
+let imagesToLoad = document.querySelectorAll('img[data-src]');
 
-var imagesToLoad = document.querySelectorAll('img[data-src]');
-
-var loadImages = function(image) {
-	image.setAttribute('src', image.getAttribute('data-src'));
-	image.onload = function() {
-		image.removeAttribute('data-src');
-	};
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
+
 if('IntersectionObserver' in window) {
-	var observer = new IntersectionObserver(function(items, observer) {
-		items.forEach(function(item) {
-			if(item.isIntersecting) {
-				loadImages(item.target);
-				observer.unobserve(item.target);
-			}
-		});
-	});
-	imagesToLoad.forEach(function(img) {
-		observer.observe(img);
-	});
-}
-else {
-	imagesToLoad.forEach(function(img) {
-		loadImages(img);
-	});
+const observer = new IntersectionObserver((items, observer) => {
+  items.forEach((item) => {
+    if(item.isIntersecting) {
+      loadImages(item.target);
+      observer.unobserve(item.target);
+    }
+  });
+});
+imagesToLoad.forEach((img) => {
+  observer.observe(img);
+});
+} else {
+imagesToLoad.forEach((img) => {
+  loadImages(img);
+});
 }
